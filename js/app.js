1ab6ef1fbfbe2ced648d17b17b3d9e5ef189c420
b-ellis <bradjellis18@gmail.com>
2016-07-07 14:54:15 -0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+// selects a random number between 1 and 100 //
+var rN = Math.floor((Math.random() * 100) + 1);
 
 $(document).ready(function(){
 	
+	//console.log(rN); 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +15,12 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- Start a new game upon .new click ---*/
+  	function newGame(){
+  		$(".new").click(function(){
+  			
+  		})
+  	}
 });
 
 
