ee141567e96875c1bd570f4115f828c60a714f96
Mostafa <m.elsheikh11791@gmail.com>
2016-07-11 17:29:14 +0200

    fixes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 // selects a random number between 1 and 100 //
-var rN = Math.floor((Math.random() * 100) + 1);
+
+function getRandomNumber() {
+		var rN = Math.floor((Math.random() * 100) + 1);
+		return rN;
+}
+
+function existsInArray(array, element) {
+	for (var i = 0 ; i < array.length ; i++) {
+		if (array[i] === element) {
+			return true;
+		}
+	}
+	return false;
+}
 
 $(document).ready(function(){
-	
-	console.log(rN); 
+
+	console.log(rN);
+
+	var rN = getRandomNumber();
+	var guessedNumbers = [];
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -19,47 +36,54 @@ $(document).ready(function(){
   	$(".new").click(function(){
   		location.reload();
   		//console.log('it works');
+			// clear all game logic and anything in the html relevant to the current game.
   	});
-  	
+
   	$('.button').on("click", function(event){
+
   		event.preventDefault();
   	/*--- Guess Count---*/
   		$('#count').html(function(i, val){
   		return +val+1
+  		});
 
-  	});
   	/*--- User's Guess---*/
-  	var userGuess = $('#userGuess').val();
-  	$('#userGuess').val('');
-  	/*--- Check for valid answer---*/
+	  	var userGuess = $('#userGuess').val();
+
+	  	/*--- Check for valid answer---*/
   		if(userGuess % 1 !== 0 || userGuess === ''){
   			alert("Please input a Number");
-  			return true;
+				// do something in the feedback HTML element.
+  			return;
   		}
+
   		if(userGuess < 0 || userGuess > 101){
   			alert("please input a Number between 1 and 100");
-  			return true;
+				// same as above
+  			return;
   		}
   		//if number already guesed alert user
-  	$('#guessList').append('<li>'+ userGuess +'</li>');
-  	
-  	/*--- Feedback ---*/
+			if (existsInArray(guessedNumbers, userGuess)) {
+				// alert the user somehow
+				return;
+			}
+
+	  	$('#guessList').append('<li>'+ userGuess +'</li>');
+			guessedNumbers.push(userGuess);
+
+	  	/*--- Feedback ---*/
   		if(userGuess == rN)
   			document.getElementById("feedback").innerHTML = "You Won!";
-  	
-  			else if(userGuess <= rN + 10 && userGuess >= rN - 10)
-  				document.getElementById("feedback").innerHTML = "Hot";
-  			
+			else if(userGuess <= rN + 10 && userGuess >= rN - 10)
+				document.getElementById("feedback").innerHTML = "Hot";
+			// continue logic
 
-  			//else if(userGuess <= rN + 15 && userGuess >= rN - 15);
-  				//document.getElementById("feedback").innerHTML = "Kinda Hot";
-  			
 
-  			//else if(userGuess <= rN + 20 && userGuess >= rN - 20);
-  				//document.getElementById("feedback").innerHTML = "cold";
-  	
-  			
+	  			//else if(userGuess <= rN + 15 && userGuess >= rN - 15);
+	  				//document.getElementById("feedback").innerHTML = "Kinda Hot";
+
+
+	  			//else if(userGuess <= rN + 20 && userGuess >= rN - 20);
+	  				//document.getElementById("feedback").innerHTML = "cold";
   	});
 });
-
-
